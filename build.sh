5b6caa1a713daa5a9d2a00e718e953d2e6d30631
falownik <36796295+falownik@users.noreply.github.com>
2020-01-16 16:26:22 +0100

    naprawianie budowania na czystej maszynie

---
+++ build.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+
+echo "Checking boost version"
+BOOST_VER=$(dpkg -s libboost-dev | grep "Version")
+if [ -z "${BOOST_VER}" ]
+then
+	echo "boost not found"
+	echo "Installing boost from package"
+	sudo apt-get -y install libboost-dev
+	echo "Installing boost-python3"
+	apt-get -y install libboost-python-dev
+fi
+
+
+echo "Checking python version"
+PYTHON_VER=$(python3 --version)
+if [ -z "${PYTHON_VER}" ]
+then
+	echo "python not found"
+	echo "Installing python from package"
+	sudo apt-get -y install libboost-dev
+fi
+
+
+echo "Checking make version"
+MAKE_VER=$(make --version)
+if [ -z "${MAKE_VER}" ]
+then
+	echo "python not found"
+	echo "Installing python from package"
+	sudo apt-get -y install make
+fi
+
+
+echo "Checking cmake version"
+CMAKE_VER=$(cmake --version)
+if [ -z "${CMAKE_VER}" ]
+then
+	echo "cmake not found"
+	echo "Installing cmake from package"
+	sudo apt-get -y install cmake
+fi
+
+
+echo "Checking flask version"
+CMAKE_VER=$(cmake --version)
+if [ -z "${CMAKE_VER}" ]
+then
+	echo "flask not found"
+	echo "Installing flask from package"
+	sudo apt -y install python3-flask
+fi
+
+
+GPP_VER=$(g++ --version)
+if [ -z "${GPP_VERSION}" ]
+then
+	echo "g++ not found"
+	echo "Installing g++ from package"
+	sudo apt-get -y install gpp
+fi
+
+
+
